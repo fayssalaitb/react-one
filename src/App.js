3cd2cc63f01baf61228f7d62e072f812f5054398
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-04-15 15:49:08 +0200

    start to play with some CSS inline and with classes

--- src/App.js
+++ src/App.js
@@ -34,10 +34,20 @@ class App extends Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
     return (
       <div className="App">
         <h1>Hi its React</h1>
-        <button onClick={this.switchNameHandler.bind(this, "fayssal")}>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, "fayssal")}
+        >
           Switch Name
         </button>
 
