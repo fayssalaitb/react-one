38927d76a6c066ab2f4d3f6c64f57bda78da54b8
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-04-16 12:10:34 +0200

    daynamiclly display the list and modifie the name add some commintds in the code

--- src/App.js
+++ src/App.js
@@ -3,37 +3,48 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  // the DAta i'am working with
   state = {
     persons: [
-      { name: "Max", age: 24 },
-      { name: "Manu", age: 65 },
-      { name: "Degg", age: 55 }
+      { id: "asdf1", name: "Max", age: 24 },
+      { id: "asdf2", name: "Manu", age: 65 },
+      { id: "asdf3", name: "Degg", age: 55 }
     ],
-    otherState: "some value"
+    otherState: "some value",
+    showPersons: false
+  };
+  // a function that delete person from the DOM
+  deletePersonhandler = personIndex => {
+    //Copy the array then change it
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
-  switchNameHandler = newName => {
-    //console.log('was clicked');
-    // DONT DO THIS this.state.state.persons[0].name = "Laxx";
+  //change the name of the person dinamicly
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons });
+  };
+  //toggele person list
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
     this.setState({
-      persons: [
-        { name: newName, age: 24 },
-        { name: "Manu", age: 65 },
-        { name: "Degg", age: 26 }
-      ]
+      showPersons: !doesShow
     });
   };
-  nameCangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Manu", age: 24 },
-        { name: event.target.value, age: 65 },
-        { name: "Degg", age: 26 }
-      ]
-    });
-  };
-
+  // rendering everything
   render() {
+    //some CSS styles
     const style = {
       backgroundColor: "white",
       font: "inherit",
@@ -41,32 +52,34 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer"
     };
+    // condition to check if we want to show or hide the person list
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonhandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    //what we return to the Dom after all the logic
     return (
       <div className="App">
         <h1>Hi its React</h1>
-        <button
-          style={style}
-          onClick={this.switchNameHandler.bind(this, "fayssal")}
-        >
-          Switch Name
+        <button style={style} onClick={this.togglePersonsHandler}>
+          {" "}
+          Toggle Persons
         </button>
-
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          click={this.switchNameHandler.bind(this, "tomtom")}
-          changed={this.nameCangedHandler}
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobies is Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        {persons}
       </div>
     );
   }
